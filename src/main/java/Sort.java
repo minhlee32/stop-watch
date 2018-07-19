6d1108dd1e16a4a129b4b85a8e3b4295b714264f
Minh Lee <minhlee.fre.mta@gmail.com>
2018-07-19 11:15:35 +0700

    add class Sort with method selection sort

---
+++ src/main/java/Sort.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class Sort {
+
+    private static void sort(int[] list) {
+        for (int i = 0; i < list.length; i++) {
+            int currentMin = list[i];
+            int currentMinIndex = i;
+
+            for (int j = i + 1; j < list.length; j++) {
+                if (currentMin > list[j]) {
+                    currentMin = list[j];
+                    currentMinIndex = j;
+                }
+            }
+            if (currentMinIndex != i) {
+                list[currentMinIndex] = list[i];
+                list[i] = currentMin;
+            }
+            }
+    }
+
+}
